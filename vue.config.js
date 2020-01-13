533865b450bf3921ec45e76d5e91742d1572862a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-01-13 17:30:45 +0530

    Give names to output bundles

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  publicPath: "./"
+  publicPath: "./",
+  configureWebpack: {
+    output: {
+      filename: "js/cxdashboard.js"
+    },
+    /*
+    module: {
+      rules: [
+        {
+          test: /\.json$/,
+          include: [path.resolve(__dirname, "src", "assets", "i18n")],
+          use: ["banana-i18n-loader"]
+        }
+      ]
+    },*/
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          libs: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "cxdashboard.libs",
+            chunks: "all"
+          }
+        }
+      }
+    }
+  }
 };
